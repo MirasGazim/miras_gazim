ec2b577c03d06e5884541c38cdf459165df3369a
Газимов Диас <gvzumdias@gmail.com>
2024-10-23 20:18:10 +0500

    add new word in the file

--- miras.py
+++ miras.py
@@ -1,2 +1,38 @@
-print('hello world ')
-print('hello world ')
+import random 
+SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', )
+
+# class SecondNum:
+#     def __init__(self, element):
+#         self.arr = element
+#         self.c = None
+       
+#         unique_elements = list(set(self.arr))
+        
+#         if len(unique_elements) < 2:
+#             self.c = "Недостаточно уникальных элементов для определения второго по величине." 
+#             return  
+      
+#         unique_elements.sort() 
+
+#         self.c = unique_elements[1] 
+        
+
+# walf = SecondNum([12, 45, 67, 34, 78, 1])
+# print(walf.c)
+
+# miras = SecondNum([348, 34590, 34643, 2323, 66])
+# print(miras.c)
+
+    
+# class Cat:
+
+#     def __init__(self, name, breed = 'pers', age = 1 , color = 'white'):   
+#         print('hello new object is ', self, name, breed, age, color)
+#         self.name = name 
+#         self.age = age
+#         self.breed = breed
+#         self.color = color
+
+# walt = Cat('miras')
+
+# print(walt.age)
